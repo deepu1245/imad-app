63506eac716f3c058d794126407db9bb627c4978
deepu1245 <deepakbht67@gmail.com>
2017-08-16 21:48:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,14 +80,6 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article2',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
-});
-
-app.get('/article3',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
