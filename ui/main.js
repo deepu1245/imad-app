f919fb426bb3ac48e1b687ea78a7b3f699c822ba
deepu1245 <deepakbht67@gmail.com>
2017-08-20 18:51:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,22 @@ button.onclick = function() {
           }
       }
     };
-    request.open('GET',"http://deepakbht67.imad.hasura-app.io",true);
+    request.open("GET","http://deepakbht67.imad.hasura-app.io",true);
     request.send(null);
 };
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function(){
+if (request.readyState === XMLHttpRequest.DONE) {
+//Take Some Action
+if (request.status === 200) { 
+var counter = request.responseText; 
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+} 
+} 
+}; 
+request.open("GET", "http://sethu18rr.imad.hasura-app.io/counter", true);
+request.send(); 
+console.log('finished');
+}
